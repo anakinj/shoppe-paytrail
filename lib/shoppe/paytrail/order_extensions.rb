a329990e5a0585f2c81a710ccc8893738017da62
Joakim Antman <antmanj@gmail.com>
2016-03-08 23:31:00 +0200

    Getting there

--- lib/shoppe/paytrail/order_extensions.rb
+++ lib/shoppe/paytrail/order_extensions.rb
@@ -1,11 +1,23 @@
 module Shoppe
   module Paytrail
     module OrderExtensions
-      def redirect_to_paytrail
-        # TODO: Call paytrail
+      def redirect_to_paytrail(success_url, failure_url, notification_url)
+        response = PaytrailClient::Payment.create(order_number: self.number,
+                              currency: 'EUR',
+                              locale: 'en_US',
+                              url_set: {
+                                success: success_url,
+                                failure: failure_url,
+                                notification: notification_url
+                              },
+                              price: '%.2f' % self.total)
+
+        response['url']
+      rescue
+        raise Shoppe::Errors::PaymentDeclined
       end
 
-      def accept_paytrail_payment(payment_id, token, payer_id)
+      def create_paytrail_payment
         self.payments.create(amount: self.total, method: "Paytrail", refundable: true, confirmed: false)
         self.save
       end
